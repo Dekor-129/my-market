fcdeea53cd20293081dc96d021d233289f2b219b
Dekor129 <dekor.dmitri99@gmail.com>
2022-10-31 18:19:17 +0300

    add adaptiv

--- components/ProductPage/ProductPageImg.js
+++
@@ -1,9 +0,0 @@
-
-
-
-export default function ProductImg(props) {
-  return (
-    <img className={props.className} src={props.src} width={500}
-    height={500}/>
-  )
-}
--- pages/catalog/shaped-candles/[id].js
+++ pages/catalog/shaped-candles/[id].js
@@ -1,41 +1,63 @@
-import {useRouter} from 'next/router'
-import ProductPage from '../../../components/ProductPage';
+import ProductPageImg from '../../../components/ProductPageImg';
 import db from '../../../dataBase/db.json'
 import MainContainer from '../../../Layouts/MainContainer'
 import Link from 'next/link';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
-export default function Posts({data}) {
-  const router = useRouter()
-  const product = data.filter((elem)=> elem.name === router.query.id)[0]
+export default function Posts({product}) {
 
   return (
-    <>
-      <MainContainer title = { product.text }>
-        <Container>
-          <div className='blockPath'>
-            <Link href={'/'}>
-              <a>Главная / </a>
-            </Link>
-            <Link href={'/catalog'}>
-              <a>Каталог / </a>
-            </Link>
-            <Link href={'/catalog/shaped-candles'}>
-              <a>Форменые свечи / </a>
-            </Link>
-            <Link href={`/catalog/shaped-candles/${router.query.id}`}>
-              <a><span>{product.text}</span></a>
-            </Link>
-          </div>
-        </Container>
-        <ProductPage src={ product.src }/>
-      </MainContainer>
-    </>
+    <MainContainer title = { product.text }>
+      <Container>
+        <div className='blockPath'>
+          <Link href={'/'}>
+            <a>Главная / </a>
+          </Link>
+          <Link href={'/catalog'}>
+            <a>Каталог / </a>
+          </Link>
+          <Link href={'/catalog/shaped-candles'}>
+            <a>Форменые свечи / </a>
+          </Link>
+          <Link href={`/catalog/shaped-candles/${product.name}`}>
+            <a><span>{product.text}</span></a>
+          </Link>
+        </div>
+      </Container>
+
+      <Container className='my-4'>
+        <Row>
+          <Col lg={6}>
+            <ProductPageImg src={ product.src }/>
+          </Col>
+          <Col lg={6}>
+            <Tabs
+              defaultActiveKey="profile"
+              id="uncontrolled-tab-example"
+              className="mb-3"
+            >
+              <Tab eventKey="home" title="Home">
+                Text
+              </Tab>
+              <Tab eventKey="profile" title="Profile">
+                Text
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+
+      </Container>
+    </MainContainer>
   )
 }
 
 export async function getServerSideProps(context) {
+  const router = context.query.id
+  const product = await db.filter((elem)=> elem.name === router)[0]
+
   return {
-    props: {data: db}, // will be passed to the page component as props
+    props: { product }, // will be passed to the page component as props
   }
 }
