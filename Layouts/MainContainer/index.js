420d8b4ac51bac6fa90b8016a5cbca410150d936
Dekor129 <dekor.dmitri99@gmail.com>
2022-11-15 00:28:27 +0300

    update Product Page and connect telegramm bot

--- Layouts/MainContainer/index.js
+++ Layouts/MainContainer/index.js
@@ -8,6 +8,7 @@ export default function MainContainer(props) {
         <title>{props.title}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="icon" href="public/favicon.ico" type="image/x-icon" />
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
       </Head>
       <Header />
